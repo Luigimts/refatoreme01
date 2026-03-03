c48a6c22ddc87f3a1f3f890807aaf969669d1cb4
Luigi Mattos <luigi.simao@aluno.senai.br>
2026-03-03 16:19:28 -0300

    Iniciando fatoracao para React

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Services />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
