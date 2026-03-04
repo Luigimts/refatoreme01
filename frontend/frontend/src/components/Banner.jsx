aaf17053f8338327c1f37d24d83e18eeba54e32a
Luigi Mattos <luigi.simao@aluno.senai.br>
2026-03-03 22:41:03 -0300

    Refatorando projeto para React com Docker

---
+++ frontend/frontend/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+function Banner() {
+  return (
+    <section>
+      <h2>Soluções Inovadoras em TI</h2>
+      <p>Transformando ideias em arquiteturas escaláveis e interfaces modernas.</p>
+      <button>Saiba Mais</button>
+    </section>
+  );
+}
+
+export default Banner;
---
+++ frontend/frontend/src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>TechNova</h1>;
+}
+
+export default Header;
